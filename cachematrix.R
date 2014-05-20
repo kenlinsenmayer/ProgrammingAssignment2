c394d402a0dcaf5c03549f27fdf7abfabd9a320b
Ken Linsenmayer <klinsenmayer@me.com>
2014-05-20 15:07:56 +0200

    updated documentation

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,8 @@
 ## Functions to create special matrix that supports
 ## caching the solution of the inverse function
 
-## create a matrix that supports caching of the inverse
+## Create a matrix that supports caching of the inverse
+## inv will hold the value of the cached result
 
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
@@ -21,11 +22,12 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
     inv <- x$getInv()
+    ## First check to see if a cached value exists
     if(!is.null(inv)) {
         message("getting cached data")
         return(inv)
     }
-    ## otherwise...
+    ## otherwise, calculate the inverse, cache it, and return result
     mat <- x$get()
     inv <- solve(mat)
     x$setInv(inv)
